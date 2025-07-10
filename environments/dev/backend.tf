c7f380105f702f0aa1654f517eb5ea7e21919540
1EyedRabbit <34923351+1EyedRabbit@users.noreply.github.com>
2025-07-10 13:40:02 +0530

    Tried to modify the backend bucket name

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,3 @@
 terraform {
-  backend "gcs" {
-    bucket = "cts01-pratikkamble-tfstate"
-    prefix = "env/dev/${var.build_id}"
-  }
+  backend "gcs" {}
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,3 @@
 terraform {
-  backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
-    prefix = "env/prod/${var.build_id}"
-  }
+  backend "gcs" {}
 }
